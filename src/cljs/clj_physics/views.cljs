39c875985e5ae22faa337e13240562f62dea05b7
Jon McDuffie <jon@jrmcduffie.com>
2020-06-02 20:48:43 -0400

    Get some basic kinematics working

--- src/cljs/clj_physics/views.cljs
+++ src/cljs/clj_physics/views.cljs
@@ -8,17 +8,10 @@
 (defonce interval (r/atom nil))
 (defonce state
   (r/atom {:particles [{:x 0 :y 0
-                        :vx 1 :vy 1}
-                       {:x 10 :y 0
-                        :vx 1 :vy 1}
-                       {:x 11 :y 5
-                        :vx 1 :vy 1}
-                       {:x 0 :y 4
-                        :vx 1 :vy 1}
-                       {:x 30 :y 5
-                        :vx 1 :vy 1}]
-           :max-x 100
-           :max-y 100}))
+                        :vx 2 :vy 1}]
+           :delta-t 0}))
+(def canvas-height 150)
+(def canvas-width 300)
 
 
 (defn clear-canvas [canvas]
@@ -31,47 +24,59 @@
       (.rect 0 0 w h)
       (.fill))))
 
-(defn get-new-y [y vy]
-  (if (> y (:max-y @state))
-    {:y (- y vy) :vy (* -1 vy)}
-    (if (< y 0)
-      {:y (- y vy) :vy (* -1 vy)}
-      {:y (+ vy y) :vy vy})))
+(defn delta-y [vy delta-t]
+  (+ (/ (* delta-t delta-t -1) 2)
+     (* vy delta-t)))
 
-(defn get-new-x [x vx]
-  (if (> x (:max-x @state))
-    {:x (- x vx) :vx (* -1 vx)}
-    (if (< x 0)
-      {:x (- x vx) :vx (* -1 vx)}
-      {:x (+ vx x) :vx vx})))
+(defn delta-x [vx delta-t]
+  (* vx (rem delta-t 100)))
 
 (defn update-particles [state]
-  (assoc state :particles
-         (mapv (fn [{:keys [x y vx vy]}]
-                 (let [{:keys [x vx]} (get-new-x x vx)
-                       {:keys [y vy]} (get-new-y y vy)]
-                   {:x x :vx vx :y y :vy vy}))
-               (:particles state))))
+  (-> state
+      (update :delta-t (fn [t] (+ t (/ 1 60))))
+      (assoc :particles
+             (mapv (fn [{:keys [vx vy]}]
+                     (let [delta-t (:delta-t state)
+                           x (delta-x vx delta-t)
+                           y (delta-y vy delta-t)]
+                       {:x x :vx vx :y y :vy vy}))
+                   (:particles state)))))
 
 (defn update-world []
-  (swap! state update-particles)
-  (println "Updating positions"))
+  (swap! state update-particles))
+
+(defn draw-line [ctx [x1 y1] [x2 y2]]
+  (doto ctx
+    (.beginPath)
+    (.moveTo x1 y1)
+    (.lineTo x2 y2)
+    (.stroke)))
+
+(defn draw-axes [ctx]
+  (draw-line ctx
+             [0 canvas-height]
+             [0 0])
+  (draw-line ctx
+             [0 canvas-height]
+             [canvas-width canvas-height]))
 
 (defn paint [canvas ctx]
   (let [particles (:particles @state)]
-    (println "Painting the screen")
     (clear-canvas canvas)
-    (doseq [{:keys [x y vx vy]} particles]
-      (println x y vx vy)
+    (draw-axes ctx)
+    (doseq [{:keys [x y]} particles]
       (set! (.-fillStyle ctx) "black")
       (doto ctx
         (.beginPath)
-        (.rect x y 2 2)
+        (.rect x (- canvas-height y) 2 2)
         (.fill)))))
 
 (defn tick [canvas ctx]
   (update-world)
   (paint canvas ctx))
+
+(defn reset-world [state]
+  (assoc state :delta-t 0))
 
 (defn canvas-page []
   (r/create-class
@@ -85,7 +90,8 @@
         (reset! interval (js/setInterval #(tick canvas ctx) (/ 1000 60)))))
     :reagent-render
     (fn []
-      [:canvas#drawing1])}))
+      [:canvas#drawing1 {:style {:height canvas-height
+                                 :width  canvas-width}}])}))
 
 (defn home-panel []
   (let [name (re-frame/subscribe [::subs/name])]
@@ -96,6 +102,27 @@
       [:a {:href "#/about"}
        "go to About Page"]]
 
+     [:div
+      [:label
+       "vy0"
+       [:input {:type "number"
+                :value (get-in @state [:particles 0 :vy])
+                :on-change #(swap! state assoc-in [:particles 0 :vy]
+                                   (-> % .-target .-value))}]]
+      [:label
+       "vx0"
+       [:input {:type "number"
+                :value (get-in @state [:particles 0 :vx])
+                :on-change #(swap! state assoc-in [:particles 0 :vx]
+                                   (-> % .-target .-value))}]]
+      [:label
+       "delta t"
+       [:input {:type "number"
+                :readonly true
+                :value (.toFixed (get @state :delta-t) 2)}]]
+      [:button
+       {:on-click #(swap! state reset-world)}
+       "set t = 0"]]
      [canvas-page]]))
 
 
